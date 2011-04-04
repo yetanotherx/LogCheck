f461e5f37da2036f705425d2c21f9a18786d3685
Yetanotherx <yetanotherx@gmail.com>
2011-04-04 12:18:16 -0400

    Initial import

---
+++ src/yetanotherx/util/LogCheck/LogCheck.java
@@ -0,0 +1,40 @@
+package yetanotherx.util.LogCheck;
+
+import java.io.File;
+import java.io.IOException;
+
+public class LogCheck {
+
+    /**
+     * @param args
+     * @throws IOException 
+     */
+    public static void main(String[] args) throws IOException {
+
+	File file = new File("server/server.log");
+
+	if( !file.exists() ) {
+	    System.out.println( "ERROR: Could not find server/server.log!!!" );
+	    System.exit(1);
+	}
+
+	long lastModified = file.lastModified();
+	long currentTime = System.currentTimeMillis();
+
+	//900000 ms = 15 minutes
+	if( currentTime - lastModified > 900000 ) {
+	    //It's been 15 minutes, something went wrong.
+	    Runtime.getRuntime().exec("killall java");
+	    Runtime.getRuntime().exec("server/start");
+	    System.out.println( "I had to kill the process..." );
+	    System.exit(1);
+	}
+	else {
+	    //Nothing wrong.
+	    System.out.println( "Server appears to be up." );
+	    System.exit(0);
+	}
+
+    }
+
+}
